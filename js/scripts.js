bf1b25301d3f52339fbe10fd2ca87be89e185f43
Dustin Herboldshimer and Kerriann Walker <kerriann@>
2019-04-04 12:09:05 -0700

    add logic structure for creating symbols

--- js/scripts.js
+++ js/scripts.js
@@ -20,6 +20,7 @@ function convertToRoman(value){
 
 
   function placeCounter(number, n) {
+
     return Math.floor((number / Math.pow(10, n-1)) %10);
   }
 
@@ -32,8 +33,49 @@ function getNumOfDigits(input) {
 
 
 
+function digitToSymbol(input){
+  var numDigits = getNumOfDigits(input);
+  var placeValues = [];
+  for (i = 1; i <= numDigits; i++) {
+    placeValues.unshift(placeCounter(input, i));
 
 
+    console.log(placeCounter(input, i));
+    console.log(placeValues);
+  }
+
+  for(i = 0; i <= placeValues.length; i ++){
+    if (placeValues.length)
+  }
+  switch (numDigits) {
+  case 1:
+    //we know its a one's digit.
+    //Find the roman numeral for the digit value;
+    // return symOnes;
+    break;
+  case 2:
+    //we know its a ten's digit
+    //find the romal numeral for the 10's and 1's;
+    //return symOnes + symTens;
+    break;
+  case 2:
+     day = "Tuesday";
+    break;
+  case 3:
+    day = "Wednesday";
+    break;
+  case 4:
+    day = "Thursday";
+    break;
+}
+}
+
+function symOnes(value){
+  switch(value) {
+    case 1 :
+      return "I";
+      break;
+}
 
 
 
@@ -47,13 +89,9 @@ $(document).ready(function(){
       // console.log(convertToRoman(input));
       // console.log(placeCounter(input, 2));
       // console.log(getNumOfDigits(input));
+      digitToSymbol(input);
+    });
 
-      var numDigits = getNumOfDigits(input);
-      for (i = 1; i <= numDigits; i++) {
-        console.log(placeCounter(input, i));
-      }
-
-   });
 
 
 
